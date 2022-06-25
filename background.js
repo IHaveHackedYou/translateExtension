66e805cf93b97e2ae20427565107347cd163d299
flopp <hackeranonym@protonmail.com>
2022-06-25 11:37:58 +0200

    improved content.js performance massively

--- background.js
+++ background.js
@@ -30,62 +30,72 @@ chrome.runtime.onConnect.addListener(function (port) {
 });
 
 function refreshLists(reload = false) {
+  // words that should get translated
   words = [];
+  // all words of wordList
   allWords = [];
+  // translations in German
   translations = [];
+  // word frequency number in English language
   counts = [];
 
+  // load word list from server/github as txt file
   fetch(URL).then((response) => {
     response.text().then((output) => {
+      // load englishLevel: float of english level of user, the smaller the number the more words knows the user
+      // load dislikeWordList: words that user marked with X or disliked, user already know the word
       chrome.storage.sync.get(
         ["englishLevel", "dislikeWordList"],
         ({ englishLevel, dislikeWordList }) => {
-          splitted_text = output.split("\n");
-          // TODO remove change complete.txt
-          englishLevel = 1 / englishLevel / 100;
+          // split word list into array
+          splittedWordList = output.split("\n");
 
-          if (dislikeWordList === undefined) {
-            dislikeWordList = ["jfijeow0/98f9whf78w6732()/&%$/&("];
-          }
-          var i = 0,
-            len = splitted_text.length;
-
+          // iterate over splittedWordList
+          var i = 0;
+          len = splittedWordList.length;
           while (i < len) {
-            further_splitted_text = splitted_text[i].split(",");
-            if (
-              further_splitted_text[1] < englishLevel &&
-              !dislikeWordList.includes(further_splitted_text[2])
-            ) {
-              words.push(further_splitted_text[2]);
-              translations.push(further_splitted_text[3]);
-              counts.push(further_splitted_text[1]);
+            // split further ...
+            furtherSplittedText = splittedWordList[i].split(",");
+
+            // into
+            count = furtherSplittedText[1];
+            word = furtherSplittedText[2];
+            translation = furtherSplittedText[3];
+
+            // check if current counts should get translated (smaller as englishLevel)
+            // and look for duplicate in dislikeWordList
+            if (count < englishLevel && !dislikeWordList.includes(word)) {
+              words.push(word);
+              translations.push(translation);
+              counts.push(count);
             }
-            allWords.push(further_splitted_text[2]);
+            allWords.push(word);
             i++;
           }
+
+          // num of words that got disliked
           sumDislikedCounts = 0;
+          // iterate over dislikeWordList
           for (let i = 0; i < dislikeWordList.length; i++) {
             index = allWords.indexOf(dislikeWordList[i]);
-            if (index > -1 && counts[index]!==undefined) {
+            // if index is valid
+            if (index > -1 && counts[index] !== undefined) {
               sumDislikedCounts += +counts[index];
             }
           }
-          if(dislikeWordList.length > 0)
+          if (dislikeWordList.length > 0) {
+            // new englishLevel is average of counts of disliked words
             englishLevel = sumDislikedCounts / dislikeWordList.length;
-          console.log("englishLevel: " + englishLevel);
-          while (i < len) {
-            further_splitted_text = splitted_text[i].split(",");
-            if (
-              further_splitted_text[1] < englishLevel &&
-              !dislikeWordList.includes(further_splitted_text[2])
-            ) {
-              words.push(further_splitted_text[2]);
-              translations.push(further_splitted_text[3]);
-            }
-            i++;
+          } else {
+            englishLevel = 100000000000;
           }
+
+          // new englishLevel gets fist applied when this function runs again, due to performance reasons
+          console.log("englishLevel: " + englishLevel);
+
+          // save lists
           chrome.storage.local.set({ words });
-          chrome.storage.sync.set({englishLevel});
+          chrome.storage.sync.set({ englishLevel });
           chrome.storage.local.set({ translations }, () => {
             console.log("refreshed");
             if (reload) chrome.tabs.reload();
--- content.js
+++ content.js
@@ -1,23 +1,3 @@
-// local url to complete.txt
-// const URL = chrome.runtime.getURL("complete.txt");
-const URL = "https://raw.githubusercontent.com/IHaveHackedYou/translateExtension/main/complete.txt";
-
-// like button with css code
-const LIKE_BUTTON_HTML_TEXT = `<button style="
-border: 0px;
-width: 1.2em; 
-height: 1.2em;
-background-color: #8AB4F8;
-border-radius: 20%;
-vertical-align: middle;
-margin: 0px 0px 3px 2px;
-text-align: center;
-line-height: 10px;
-color: black;
-padding: 0px 0px;
-// font-size: 10px;
-" class="like_button_69420">✔</button>`;
-
 // dislike button with css code
 const DISLIKE_BUTTON_HTML_TEXT = `<button style="
 border: 0px;
@@ -38,255 +18,183 @@ const STEALTH_MODE_HTML_END = `</i>`;
 const NON_STEALTH_MODE_HTML_BEGIN = ` <b>`;
 const NON_STEALTH_MODE_HTML_END = `</b>`;
 
-raw_text = "";
 words = [];
-counts = [];
 translations = [];
-let translate_threshold = 0.0002;
 
-// open the file
-// console.log("start OLD " + Date.now());
-// fetch(URL).then((response) =>
-//     response.text().then((output) => {
-//         chrome.storage.sync.get(["englishLevel", "feedbackCheckboxChecked", "stealthModeCheckboxChecked"], ({ englishLevel, feedbackCheckboxChecked, stealthModeCheckboxChecked }) => {
-//             console.log("loaded files" + Date.now());
-//             translate_threshold = 1 / englishLevel / 100;
-//             raw_text = output.toLowerCase();
-
-//             if (feedbackCheckboxChecked) {
-//                 button_html_like = LIKE_BUTTON_HTML_TEXT;
-//                 button_html_dislike = DISLIKE_BUTTON_HTML_TEXT;
-//             } else {
-//                 button_html_like = " ";
-//                 button_html_dislike = " ";
-//             }
-
-//             if (stealthModeCheckboxChecked) {
-//                 trans_html_begin = STEALTH_MODE_HTML_BEGIN;
-//                 trans_html_end = STEALTH_MODE_HTML_END;
-//             } else {
-//                 trans_html_begin = NON_STEALTH_MODE_HTML_BEGIN;
-//                 trans_html_end = NON_STEALTH_MODE_HTML_END;
-//             }
-//             // split file in lines
-//             splitted_text = raw_text.split("\n");
-
-//             array = [];
-
-//             for (let i = 1; i < splitted_text.length; i++) {
-//                 // split line further into words[2], counts[1], translations[3]
-//                 further_splitted_text = splitted_text[i].split(",")
-//                 count_to_add = further_splitted_text[1];
-//                 // check whether word should get translated
-//                 if (count_to_add < translate_threshold) {
-//                     word_to_add = further_splitted_text[2]
-//                     word_to_add = word_to_add.replace("\r", "")
-//                     counts.push(count_to_add)
-//                     words.push(word_to_add.toString());
-//                     translation_to_add = further_splitted_text[3]
-//                         // translation_to_add = translation_to_add.substring(0, translation_to_add.length - 1);
-//                     translations.push(translation_to_add);
-//                 }
-//             }
-//             console.log("splitted loaded text" + Date.now());
-//             // all p paragraphs in the page
-//             const text = document.querySelectorAll("p, blockquote, pre");
-//             founded_words = [];
-//             for (let i = 0; i < text.length; i++) {
-//                 text[i].innerHTML.split(" ").forEach((word) => {
-//                     // iterate over every word per paragraph
-//                     if (
-//                         words.includes(word.toString()) &&
-//                         !founded_words.includes(word.toString())
-//                     ) {
-//                         founded_words.push(word.toString());
-//                         // console.log(word.toString());
-//                     }
-//                 });
-//             }
-//             console.log("calculated " + Date.now());
-//             for (let i = 0; i < text.length; i++) {
-//                 current_text = text[i].innerHTML;
-//                 for (let i = 0; i < founded_words.length; i++) {
-//                     index = words.indexOf(founded_words[i]);
-
-//                     if (!(translations[index] === "") && translations[index] && translations[index].length > 0) {
-//                         current_text = current_text.replaceAll(
-//                             " " + founded_words[i] + " ",
-//                             " " + founded_words[i] + trans_html_begin + translations[index] + trans_html_end + button_html_like + button_html_dislike
-//                         );
-//                         current_text = current_text.replaceAll(
-//                             " " + founded_words[i] + ".",
-//                             " " + founded_words[i] + trans_html_begin + translations[index] + trans_html_end + ". " + button_html_like + button_html_dislike
-//                         );
-//                         current_text = current_text.replaceAll(
-//                             " " + founded_words[i] + ",",
-//                             " " + founded_words[i] + trans_html_begin + translations[index] + trans_html_end + ", " + button_html_like + button_html_dislike
-//                         );
-//                     }
-//                     // text[i].textContent = text[i].textContent.replaceAll(
-//                     //     founded_words[i],
-//                     //     founded_words[i] + "(edit)"
-//                     // );
-//                     // founded_words.splice(i, 1);
-//                 }
-//                 // console.log(current_text);
-//                 text[i].innerHTML = current_text;
-
-//             }
-//             console.log("replaced " + Date.now());
-//         });
-
-//         let like_buttons = document.getElementsByClassName("like_button_69420");
-//         let dislike_buttons = document.getElementsByClassName("dislike_button_69420");
-//         for (let i = 0; i < like_buttons.length; i++) {
-//             like_buttons[i].addEventListener("click", function() {
-//                 console.log("like")
-//                 console.log(like_buttons[i].previousSibling.innerHTML)
-//                     // word regarding to button
-//                 current_word = like_buttons[i].previousSibling.innerHTML
-//             });
-//         }
-//         for (let i = 0; i < dislike_buttons.length; i++) {
-//             dislike_buttons[i].addEventListener("click", function() {
-//                 console.log("dislike")
-//                 current_word = dislike_buttons[i].previousSibling.previousSibling.innerHTML
-//                 chrome.storage.sync.get("dislike_word_list", ({ dislike_word_list }) => {
-//                     console.log(dislike_word_list)
-//                     if (!dislike_word_list.includes(current_word)) {
-//                         dislike_word_list.push(current_word.toString())
-//                         chrome.storage.sync.set({ dislike_word_list });
-//                     }
-//                 });
-
-
-//             });
-//         }
-//     })
-// );
-
-
-//----------------------------------NEW----------------------------------
-
-console.log("start NEW" + Date.now())
-chrome.storage.local.get(["words", "translations"], ({ words, translations }) => {
-    chrome.storage.sync.get(["feedbackCheckboxChecked", "stealthModeCheckboxChecked"], ({ feedbackCheckboxChecked, stealthModeCheckboxChecked }) => {
-        console.log("loaded files" + Date.now())
+console.log("start" + Date.now());
+startTime = Date.now();
+chrome.storage.local.get(
+  ["words", "translations"],
+  ({ words, translations }) => {
+    chrome.storage.sync.get(
+      ["feedbackCheckboxChecked", "stealthModeCheckboxChecked"],
+      ({ feedbackCheckboxChecked, stealthModeCheckboxChecked }) => {
+        console.log("loaded files in: " + (Date.now() - startTime) + "ms");
 
         if (feedbackCheckboxChecked) {
-            button_html_like = LIKE_BUTTON_HTML_TEXT;
-            button_html_dislike = DISLIKE_BUTTON_HTML_TEXT;
+          button_html_dislike = DISLIKE_BUTTON_HTML_TEXT;
         } else {
-            button_html_like = " ";
-            button_html_dislike = " ";
+          button_html_like = " ";
+          button_html_dislike = " ";
         }
 
         if (stealthModeCheckboxChecked) {
-            trans_html_begin = STEALTH_MODE_HTML_BEGIN;
-            trans_html_end = STEALTH_MODE_HTML_END;
+          trans_html_begin = STEALTH_MODE_HTML_BEGIN;
+          trans_html_end = STEALTH_MODE_HTML_END;
         } else {
-            trans_html_begin = NON_STEALTH_MODE_HTML_BEGIN;
-            trans_html_end = NON_STEALTH_MODE_HTML_END;
+          trans_html_begin = NON_STEALTH_MODE_HTML_BEGIN;
+          trans_html_end = NON_STEALTH_MODE_HTML_END;
         }
-
+        // get all text from website with "p" or "blockquote" tag
         const text = document.querySelectorAll("p, blockquote");
-        var counter = 0;
-        founded_words = [];
-        // var i = 0,
-        //     len = text.length;
-        // while (i < len) {
-        //     txt_words = text[i].innerHTML.split(" ");
-        //     var j = 0,
-        //         len2 = words.length;
-        //     while (j < len2) {
-        //         word = txt_words[j];
-        //         if (words.includes(word) && !founded_words.includes(word)) {
-        //             founded_words.push(word);
-        //         }
-        //         counter++;
-        //         j++;
-        //     }
-        //     i++;
-        //     counter++;
-        // }
-        for (let i = 0; i < text.length; i++) {
-            text[i].textContent.split(" ").forEach((word) => {
-                // iterate over every word per paragraph
-                if (!founded_words.includes(word.toString()) &&
-                    words.includes(word.toString())
-                ) {
-                    founded_words.push(word.toString());
-                    // console.log(word.toString());
-                }
-                counter++;
-            });
-            counter++;
-        }
 
-        console.log("calculated founded words" + counter + "  " + Date.now())
+        foundedWords = [];
+        startWordComparing = Date.now();
+        i = 0;
+        while (i < text.length) {
+          j = 0;
+          currentText = text[i].textContent;
+          textWords = currentText.split(" ");
+          while (j < textWords.length) {
+            foundedWord = textWords[j];
+            index = words.indexOf(foundedWord);
+            if (index > -1) {
+              // foundedWords.push(textWords[j].toString());
 
-
-        for (let i = 0; i < text.length; i++) {
-            current_text = text[i].innerHTML;
-            for (let i = 0; i < founded_words.length; i++) {
-                index = words.indexOf(founded_words[i]);
-
-                if (!(translations[index] === "") && translations[index] && translations[index].length > 0) {
-                    current_text = current_text.replaceAll(
-                        " " + founded_words[i] + " ",
-                        " <span>" + founded_words[i] + "</span>" + trans_html_begin + translations[index] + trans_html_end + button_html_dislike //+ button_html_dislike
-                    );
-                    current_text = current_text.replaceAll(
-                        " " + founded_words[i] + ".",
-                        " <span>" + founded_words[i] + "</span>" + trans_html_begin + translations[index] + trans_html_end + ". " + button_html_dislike //+ button_html_dislike
-                    );
-                    current_text = current_text.replaceAll(
-                        " " + founded_words[i] + ",",
-                        " <span>" + founded_words[i] + "</span>" + trans_html_begin + translations[index] + trans_html_end + ", " + button_html_dislike //+ button_html_dislike
-                    );
-                }
-                // text[i].textContent = text[i].textContent.replaceAll(
-                //     founded_words[i],
-                //     founded_words[i] + "(edit)"
-                // );
-                // founded_words.splice(i, 1);
+              currentText = currentText.replace(
+                " " + foundedWord + " ",
+                " <span>" +
+                  foundedWord +
+                  "</span>" +
+                  trans_html_begin +
+                  translations[index] +
+                  trans_html_end +
+                  button_html_dislike
+              );
+              currentText = currentText.replace(
+                " " + foundedWord + ".",
+                " <span>" +
+                  foundedWord +
+                  "</span>" +
+                  trans_html_begin +
+                  translations[index] +
+                  trans_html_end +
+                  ". " +
+                  button_html_dislike
+              );
+              currentText = currentText.replace(
+                " " + foundedWord + ",",
+                " <span>" +
+                  foundedWord +
+                  "</span>" +
+                  trans_html_begin +
+                  translations[index] +
+                  trans_html_end +
+                  ", " +
+                  button_html_dislike
+              );
             }
-            text[i].innerHTML = current_text;
+            j++;
+          }
+          text[i].innerHTML = currentText;
+
+          i++;
         }
-        console.log("replaced website " + Date.now())
+        console.log(
+          "compared words in: " + (Date.now() - startWordComparing) + "ms"
+        );
+
+        // startWordReplacing = Date.now();
+        // for (let i = 0; i < text.length; i++) {
+        //   current_text = text[i].innerHTML;
+        //   for (let i = 0; i < foundedWords.length; i++) {
+        //     index = words.indexOf(foundedWords[i]);
+
+        //     if (
+        //       !(translations[index] === "") &&
+        //       translations[index] &&
+        //       translations[index].length > 0
+        //     ) {
+        //       current_text = current_text.replace(
+        //         " " + foundedWords[i] + " ",
+        //         " <span>" +
+        //           foundedWords[i] +
+        //           "</span>" +
+        //           trans_html_begin +
+        //           translations[index] +
+        //           trans_html_end +
+        //           button_html_dislike
+        //       );
+        //       current_text = current_text.replace(
+        //         " " + foundedWords[i] + ".",
+        //         " <span>" +
+        //           foundedWords[i] +
+        //           "</span>" +
+        //           trans_html_begin +
+        //           translations[index] +
+        //           trans_html_end +
+        //           ". " +
+        //           button_html_dislike
+        //       );
+        //       current_text = current_text.replace(
+        //         " " + foundedWords[i] + ",",
+        //         " <span>" +
+        //           foundedWords[i] +
+        //           "</span>" +
+        //           trans_html_begin +
+        //           translations[index] +
+        //           trans_html_end +
+        //           ", " +
+        //           button_html_dislike
+        //       );
+        //     }
+        //   }
+        //   startReplacingTime = Date.now()
+        //   // set replaced text as website text
+        //   text[i].innerHTML = current_text;
+        //   console.log("replaced single paragraph in: " + (Date.now() - startReplacingTime) + "ms")
+        // }
+        // console.log(
+        //   "replaced complete website in: " + (Date.now() - startWordReplacing) + "ms"
+        // );
+        console.log("complete duration: " + (Date.now() - startTime) + "ms");
 
         // let likeButtons = document.getElementsByClassName("like_button_69420");
-        let dislikeButtons = document.getElementsByClassName("dislike_button_69420");
-        console.log(dislikeButtons.length)
-        // for (let i = 0; i < likeButtons.length; i++) {
-        //     likeButtons[i].addEventListener("click", function() {
-        //         console.log("like")
-        //         console.log(likeButtons[i].previousSibling.previousSibling.innerHTML)
-        //             // word regarding to button
-        //         current_word = likeButtons[i].previousSibling.innerHTML
-        //     });
-        // }
+        let dislikeButtons = document.getElementsByClassName(
+          "dislike_button_69420"
+        );
+
         for (let i = 0; i < dislikeButtons.length; i++) {
-            dislikeButtons[i].addEventListener("click", function() {
-                currentWord = dislikeButtons[i].previousElementSibling.previousElementSibling.textContent
-                console.log(currentWord)
-                chrome.storage.sync.get("dislikeWordList", ({ dislikeWordList }) => {
-                    if (dislikeWordList === undefined) {
-                        dislikeWordList = ["jfijeow0/98f9whf78w6732()/&%$/&("];
-                    }
-                    if (!dislikeWordList.includes(currentWord)) {
-                        dislikeWordList.push(currentWord.toString())
-                        chrome.storage.sync.set({ dislikeWordList });
-                        dislikeButtons[i].style.backgroundColor = "#636466";
-                        console.log(dislikeWordList)
-                    } else {
-                        dislikeWordList.splice(dislikeWordList.indexOf(currentWord), 1)
-                        chrome.storage.sync.set({ dislikeWordList });
-                        dislikeButtons[i].style.backgroundColor = "#8AB4F8";
-                        console.log(dislikeWordList)
-                    }
-                });
-            });
-        };
-    });
-});
+          dislikeButtons[i].addEventListener("click", function () {
+            currentWord =
+              dislikeButtons[i].previousElementSibling.previousElementSibling
+                .textContent;
+            console.log(currentWord);
+            chrome.storage.sync.get(
+              "dislikeWordList",
+              ({ dislikeWordList }) => {
+                if (dislikeWordList === undefined) {
+                  dislikeWordList = ["jfijeow0/98f9whf78w6732()/&%$/&("];
+                }
+                if (!dislikeWordList.includes(currentWord)) {
+                  dislikeWordList.push(currentWord.toString());
+                  chrome.storage.sync.set({ dislikeWordList });
+                  dislikeButtons[i].style.backgroundColor = "#636466";
+                  console.log(dislikeWordList);
+                } else {
+                  dislikeWordList.splice(
+                    dislikeWordList.indexOf(currentWord),
+                    1
+                  );
+                  chrome.storage.sync.set({ dislikeWordList });
+                  dislikeButtons[i].style.backgroundColor = "#8AB4F8";
+                  console.log(dislikeWordList);
+                }
+              }
+            );
+          });
+        }
+      }
+    );
+  }
+);
